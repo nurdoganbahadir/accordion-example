d96b6e1309de0296be9e74242ee55b06072987e1
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-03 16:46:33 +0300

    proje tamamlandı buton yapıları düzgün çalışıyor ve data card yapısına entegre edildi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <div className="container">
-        <h3>questions and answer about login</h3>
+        <h3>QUESTİONS AND ANSWERS ABOUT LOGİN</h3>
         <section className="info">
           {questions.map((question) => {
             return <SingleQuestion key={question.id} {...question} />;
