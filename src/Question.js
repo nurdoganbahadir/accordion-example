044d3e6970041058d89dffd2cad4a228b2d96fe5
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-03 16:41:33 +0300

    file created

---
+++ src/Question.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn">btn</button>
+      </header>
+      {showInfo && <p>{info}</p>}
+      <p>{info}</p>
+    </article>
+  );
+};
+
+export default Question;
