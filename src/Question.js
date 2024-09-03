656afcc589cf765f0ba3e6dfba1417991662a6f5
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-03 16:43:55 +0300

    iconlar card yapısına eklendi ve ternary kulanıldı

--- src/Question.js
+++ src/Question.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
-  const [show, setShow] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
         <button className="btn" onClick={() => setShowInfo(!showInfo)}>
-          {}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
       {showInfo && <p>{info}</p>}
